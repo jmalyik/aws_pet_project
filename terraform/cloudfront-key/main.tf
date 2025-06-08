66f5f02e1520d4e60737f49b7a675754d1c498e9
Malyik József <jozsef.malyik@barre.hu>
2025-06-08 15:08:23 +0200

    github wf update

--- terraform/cloudfront-key/main.tf
+++ terraform/cloudfront-key/main.tf
@@ -21,6 +21,11 @@ resource "aws_cloudfront_key_group" "cf_key_group" {
   ]
 }
 
+resource "aws_s3_bucket" "build_artifacts" {
+  bucket        = "aws-pet-project-build-artifacts"
+  force_destroy = true
+}
+
 output "cloudfront_public_key_id" {
   value = aws_cloudfront_public_key.cf_public_key.id
 }
